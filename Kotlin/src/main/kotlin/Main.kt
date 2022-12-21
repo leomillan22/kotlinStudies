feb5a7587521d82fa1db22168d68dd96e6e53581
leomillan22 <leonardomillan08@gmail.com>
2022-12-20 21:01:43 -0300

    Exercicios sobre funcoes

---
+++ Kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+/**
+ type   Bit
+ Double  64
+ float   32
+ Long    64
+ Int     32
+ Short   16
+ Byte    8
+ Boolean ?
+ String  ?
+ Char    ?
+ */
+
+
+fun main() {
+
+}
---
+++ Kotlin/src/main/kotlin/str-mathfun.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val anos: Int = 2
+    val meses: Int = anos*12
+    val dias: Int = meses*30
+    val horas: Int = dias*24
+    val minutos: Int = horas*60
+    val segundos: Int = minutos*60
+
+    println("${anos} anos equivalem a:")
+    println("${meses} meses")
+    println("${dias} dias")
+    println("${horas} horas")
+    println("${minutos} minutos")
+    println("${segundos} segundos")
+}
